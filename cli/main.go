1dd008c9c4e73460e1774fe55c68944d9a951c90
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-10-05 21:43:45 +0530

    feat: CLI entrypoint

---
+++ cli/main.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "vessel",
+	Short: "Vessel is a simple wrapper around containerd",
+	Long:  `usage: vessel [OPTIONS] COMMAND`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmd.Help()
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
