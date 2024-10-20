2dc8743d980b4d7360e1c38ded19b1d8674be3ca
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-10-20 22:03:11 +0530

    fix: Ensure task cleapup & creation

--- pkg/command/container/start.go
+++ pkg/command/container/start.go
@@ -2,6 +2,7 @@ package container
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
@@ -11,28 +12,64 @@ import (
 	"github.com/ChampionBuffalo1/vessel/pkg"
 	containerd "github.com/containerd/containerd/v2/client"
 	"github.com/containerd/containerd/v2/pkg/cio"
+	"github.com/containerd/errdefs"
 )
 
 func Start(client *containerd.Client, ctx context.Context, containerID string) error {
 	container, err := client.LoadContainer(ctx, containerID)
 	if err != nil {
+		fmt.Println("Error loading container")
 		return err
 	}
-	// Create a runtime task
-	// cio.WithStdio will attach the task's stdio to the current process's stdio
-	// The task has only been created within the container and not started
-	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+
+	task, err := container.Task(ctx, nil)
 	if err != nil {
-		return err
+		if errdefs.IsNotFound(err) {
+			// Create a runtime task
+			// cio.WithStdio will attach the task's stdio to the current process's stdio
+			// The task has only been created within the container and not started
+			task, err = container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+			if err != nil {
+				fmt.Println("Error creating task")
+				return err
+			}
+		} else {
+			fmt.Println("Error getting task")
+			return err
+		}
 	}
+	defer task.Delete(ctx) // Always delete the task as to not leave container in stopped state
+
 	// Set up a channel for waiting on task to exit
 	exitChannel, err := task.Wait(ctx)
 	if err != nil {
+		fmt.Println("Error waiting for task")
 		return err
 	}
-	// Run the container task
-	if err := task.Start(ctx); err != nil {
+
+	status, err := task.Status(ctx)
+	if err != nil {
+		fmt.Println("Error getting task status")
 		return err
+	}
+
+	if status.Status == containerd.Stopped {
+		return fmt.Errorf(`stopped container cannot be re-started. please make sure the container task is deleted.
+		You can use the command "ctr -n vessel task rm %s" to delete the task`, containerID)
+	} else if status.Status == containerd.Pausing {
+		return errors.New("please wait for the container to finish pause before resuming it")
+	} else if status.Status == containerd.Paused {
+		// Resume the container task
+		if err := task.Resume(ctx); err != nil {
+			fmt.Println("Error resuming task")
+			return err
+		}
+	} else if status.Status == containerd.Created {
+		// Run the container task
+		if err := task.Start(ctx); err != nil {
+			fmt.Println("Error starting task")
+			return err
+		}
 	}
 
 	// Setup  a handler to wait for the Ctrl + C input
@@ -41,6 +78,7 @@ func Start(client *containerd.Client, ctx context.Context, containerID string) e
 	signal.Notify(interruptC, syscall.SIGINT)
 	<-interruptC
 
+	// TODO: Handle the case where a user can run `stop` command before SIGINT is received:w
 	if err := task.Kill(ctx, syscall.SIGTERM); err != nil {
 		fmt.Println("Failed in sending sigterm")
 		return err
